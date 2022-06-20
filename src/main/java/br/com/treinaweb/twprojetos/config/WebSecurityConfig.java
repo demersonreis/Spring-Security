8beb7fc41cacda2fd8c2016c6ae21d919c3151a7
Demerson Reis <demersonsilvadosreis@gmail.com>
2022-06-20 20:28:52 -0300

    Sistema de autenticação

---
+++ src/main/java/br/com/treinaweb/twprojetos/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package br.com.treinaweb.twprojetos.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import br.com.treinaweb.twprojetos.servicos.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsServiceImpl detailsServiceImpl;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/adminlte/**").permitAll()
+		.antMatchers("/img/**").permitAll()
+		.antMatchers("/js/**").permitAll()
+		.antMatchers("/plugins/**").permitAll()
+		//.antMatchers("/funcionarios/**").permitAll()
+		.anyRequest().authenticated();
+		
+		http.formLogin()
+		.loginPage("/login")
+		.defaultSuccessUrl("/clientes")
+		.usernameParameter("email")
+		.passwordParameter("senha")
+		.permitAll();
+		
+		http.logout()
+		.logoutRequestMatcher(
+		  new AntPathRequestMatcher("/logout","GET")		
+			
+		)
+		.logoutSuccessUrl("/login");
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(detailsServiceImpl)
+		.passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+}
